a18b46c67d4abf3ced888247313f2a1c77acdd83
BahusDivus <bahusdivus@gmail.com>
2018-12-04 17:53:49 +0300

    Started writing rests

--- src/main/java/ru/bahusdivus/teleinstaBot/DbHandler.java
+++ src/main/java/ru/bahusdivus/teleinstaBot/DbHandler.java
@@ -1,5 +1,6 @@
 package ru.bahusdivus.teleinstaBot;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
 import java.util.Properties;
@@ -15,17 +16,21 @@ class DbHandler {
         return instance;
     }
 
-    private DbHandler() throws SQLException {
+    private DbHandler() {
         try (InputStream reader = this.getClass().getClassLoader().getResourceAsStream("db.properties")) {
             if (reader != null) {
                 Properties properties = new Properties();
                 properties.load(reader);
                 //String jdbcUrl = String.format("jdbc:mysql://35.226.35.187/%s?cloudSqlInstance=%s&useSSL=false", "telegrambot1db", "telegrambot1");
-                String jdbcUrl = properties.getProperty("db.link") + properties.getProperty("db.db");
-                this.connection = DriverManager.getConnection(jdbcUrl, properties.getProperty("db.user"), properties.getProperty("db.password"));
-                System.out.println("Connected");
+                String jdbcUrl = properties.getProperty("db.link") + properties.getProperty("db.db") + "?useLegacyDatetimeCode=false&serverTimezone=UTC";
+                try {
+                    this.connection = DriverManager.getConnection(jdbcUrl, properties.getProperty("db.user"), properties.getProperty("db.password"));
+                    System.out.println("Connected");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
--- src/test/java/ru/bahusdivus/teleinstaBot/DbHandlerTest.java
+++ src/test/java/ru/bahusdivus/teleinstaBot/DbHandlerTest.java
@@ -2,12 +2,15 @@ package ru.bahusdivus.teleinstaBot;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DbHandlerTest {
 
     @Test
-    void getInstance() {
-        fail("For testing purpose");
+    void getInstance() throws SQLException {
+        DbHandler db = DbHandler.getInstance();
     }
 }
